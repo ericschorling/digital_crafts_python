fc7d20566e5f773b571df491a316a60acba11d34
Eric Schorling <eric.schorling@yahoo.com>
2020-07-30 14:05:33 -0400

    updated leek

--- python_1/leet_speak.py
+++ python_1/leet_speak.py
@@ -1,10 +1,19 @@
 user_string = input("Give me a string: ")
 leet_string = ""
+leet_dict = {
+    "a" : "4",
+    "e" : "3",
+    "g" : "6",
+    "i" : "1",
+    "o" : "0",
+    "s" : "5",
+    "t" : "7",
+}
 x=0
 
 while x < len(user_string):
-    if user_string[x] == "a" or user_string[x] == "A":
-        leet_string = leet_string + ("4")
+    if user_string[x].lower() in leet_dict:
+        leet_string = leet_string + leet_dict[user_string[x].lower()]
         x += 1
     else :
         leet_string += user_string[x]
